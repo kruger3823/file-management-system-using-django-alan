2666747a6550fa5c3fd6b7591d10458b6884c655
Mubin-Hardy <mubin@codewave.com>
2022-11-05 22:15:07 +0530

    nj

--- blog/models.py
+++ blog/models.py
@@ -21,4 +21,39 @@ class Post(models.Model):
 	def get_absolute_url(self):
 		return reverse('post-detail', kwargs={'pk': self.pk})
 
+
+class News(models.Model):
+	title = models.CharField(max_length=100)
+	file = models.FileField(null=True,blank=True,upload_to='Files')
+	content = models.TextField()
+	date_posted = models.DateTimeField(default=timezone.now)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
+
+	def extension(self):
+		name, extension = os.path.splitext(self.file.name)
+		return extension
+
+	def get_absolute_url(self):
+		return reverse('post-detail', kwargs={'pk': self.pk})
+
+class Articles(models.Model):
+	title = models.CharField(max_length=100)
+	file = models.FileField(null=True,blank=True,upload_to='Files')
+	content = models.TextField()
+	date_posted = models.DateTimeField(default=timezone.now)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
+
+	def extension(self):
+		name, extension = os.path.splitext(self.file.name)
+		return extension
+
+	def get_absolute_url(self):
+		return reverse('post-detail', kwargs={'pk': self.pk})
+
         
--- blog/urls.py
+++ blog/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+path('article/new/', views.article, name='post-create-article'),
+path('news/new/', views.news, name='post-create-news'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
